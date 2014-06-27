7b356cdbe16dc279168cb64f7e818bcacd18ab26
ajaybhatia <prof.ajaybhatia@gmail.com>
2014-06-27 23:48:24 +0530

    Added PrimeNumber checker method

--- firstProject/src/FirstClass.java
+++ firstProject/src/FirstClass.java
@@ -1,6 +1,5 @@
 import java.util.Scanner;
 
-
 public class FirstClass {
 
 	/**
@@ -18,6 +17,14 @@ public class FirstClass {
 		System.out.println(a);
 		
 	
+	}
+
+	public static boolean isPrime(int n) {
+		for (int i = 2; i <= (int)Math.sqrt(n); i++)
+			if (n % i == 0)
+				return false;
+
+		return true; 	
 	}
 
 }
