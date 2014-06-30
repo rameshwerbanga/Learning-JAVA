0091bcc74820bad8c34b2b4afe57bc835384422f
rameshwerbanga <rameshwer.banga@gmail.com>
2014-06-30 21:34:54 +0530

    sum-product upto n numners

--- firstProject/src/FirstClass.java
+++ firstProject/src/FirstClass.java
@@ -7,8 +7,46 @@ public class FirstClass {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		ArrayList<Integer> list = new ArrayList<>();
+		public static void main(String[] args) {
+			System.out.println("Enter any number");
+			Scanner src = new Scanner(System.in);
+			int x = src.nextInt();
+			int pro = 1, sum = 0, temp=0, result = 0;
+			for(int i = 1; i <= x; i++){
+				temp = i;
+				while(temp > 0){
+					sum += temp % 10;
+					pro *= temp % 10;
+					temp /= 10;
+				}
+				result = sum * pro;
+				if(i == result)
+					System.out.println(result);
+				temp = 0;
+				sum = 0;
+				pro = 1;
+			}
+					
+			
+			/*int len = 0, sum = 0, temp=0 ;
+			for(int i = 100; i <= x; i++){
+				s = String.valueOf(i);
+				len = s.length();
+				temp = i;
+				while(temp > 0){
+					sum += Math.pow(temp % 10, len);
+					temp /= 10;
+				}
+				if(sum == i)
+					System.out.println(i);
+					sum = 0;
+				}
+				
+				
+			}
+			*/
+		
+		/*ArrayList<Integer> list = new ArrayList<>();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter n: ");
 		int n = scan.nextInt();
@@ -33,5 +71,7 @@ public class FirstClass {
 
 	public static int reverse(int n) {
 		return Integer.parseInt(new StringBuffer(String.valueOf(n)).reverse().toString());
-	}
+	}*/
+
+}
 }
