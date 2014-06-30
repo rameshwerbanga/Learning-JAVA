b1735ed068b48f86853e8acd14270c7b36828759
rameshwerbanga <rameshwer.banga@gmail.com>
2014-06-30 22:58:39 +0530

    frame output done

--- firstProject/src/FirstClass.java
+++ firstProject/src/FirstClass.java
@@ -8,7 +8,48 @@ public class FirstClass {
 	 * @param args
 	 */
 		public static void main(String[] args) {
-			System.out.println("Enter any number");
+			System.out.println("Enter any string");
+			Scanner scan = new Scanner(System.in);
+			String text = scan.nextLine();
+			System.out.println(text);
+			String splited[] = text.split(" ");
+			//for(String str : splited)
+				//System.out.println(str);
+			//code to find the max lenght sub string
+			int max=0, temp=0;
+			for(int i = 0; i < splited.length; i++) {
+				temp = splited[i].length();
+				if(temp >= max){
+					max = temp;
+				}	
+			}
+			String spcae;
+			for(int i = 1; i <= max+4; i++) {
+				System.out.print("*");
+			}
+			System.out.println();
+			for(String str : splited) {
+				temp = str.length();
+				if(temp < max){
+					int diff = max - temp;
+					System.out.print("* " + str);
+					for(int i = 1; i <= diff; i++){
+						System.out.print(" ");
+					}
+					System.out.println(" *");	
+				}					
+				else
+				System.out.println("* " + str + " *");	
+			}	
+			for(int i = 1; i <= max+4; i++) {
+				System.out.print("*");
+			}
+			
+
+			
+			
+			
+			/* System.out.println("Enter any number");
 			Scanner src = new Scanner(System.in);
 			int x = src.nextInt();
 			int pro = 1, sum = 0, temp=0, result = 0;
@@ -28,7 +69,7 @@ public class FirstClass {
 			}
 					
 			
-			/*int len = 0, sum = 0, temp=0 ;
+			int len = 0, sum = 0, temp=0 ;
 			for(int i = 100; i <= x; i++){
 				s = String.valueOf(i);
 				len = s.length();
