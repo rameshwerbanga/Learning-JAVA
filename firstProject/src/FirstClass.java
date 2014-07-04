7adcebb0c96f7942f3f73927b5204b38e14f4c06
rameshwerbanga <rameshwer.banga@gmail.com>
2014-07-04 22:53:06 +0530

    Ascii to char conversion

--- firstProject/src/FirstClass.java
+++ firstProject/src/FirstClass.java
@@ -8,33 +8,11 @@ public class FirstClass {
 	 * @param args
 	 */
 		public static void main(String[] args) {
-			
-	ArrayList<Integer> list = new ArrayList<>();
-		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter n: ");
-		int n = scan.nextInt();
-
-		for (int i = 2; i <= n; i++)
-			if (isPrime(i)) 
-				if(isPrime(reverse(i)))
-					list.add(reverse(i));
-	
-		Collections.sort(list);
-		Collections.reverse(list);
-		System.out.println(list);
-	}
-
-	public static boolean isPrime(int n) {
-		for (int i = 2; i <= (int)Math.sqrt(n); i++)
-			if (n % i == 0)
-				return false;
-
-		return true; 	
-	}
-
-	public static int reverse(int n) {
-		return Integer.parseInt(new StringBuffer(String.valueOf(n)).reverse().toString());
-	}
-
+			//Ascii to char conversion
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter one ascii value");
+			int alpha = input.nextInt();
+			char a = (char) alpha;
+			System.out.println(a);
+		}
 }
-
